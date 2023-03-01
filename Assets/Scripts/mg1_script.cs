77f45d6f21b886ae0326ebc25af66960e8f14e48
axelNTI <axel.thornberg@elev.ga.ntig.se>
2023-03-01 13:02:20 +0100

    Navmesh

--- Assets/Scripts/mg1_script.cs
+++ Assets/Scripts/mg1_script.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class mg1_script : MonoBehaviour
 {
-    Ray ray;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,14 +11,26 @@ public class mg1_script : MonoBehaviour
     }
 
     // Update is called once per frame
+    //Plane plane = new Plane(Vector3.up, 0);
+    public Vector3 screenPosition;
+    public Vector3 worldPosition;
+    Plane plane = new Plane(Vector3.up, 0);
+
     void Update()
     {
-        float number_of_rays = 500;
-        float totalAngle = 360;
-
-        float delta = totalAngle / number_of_rays;
-        Vector3 pos = transform.position;
-        const float magnitude = 5;
+        screenPosition = Input.mousePosition;
+        
+        Ray ray = Camera.main.ScreenPointToRay(screenPosition);
+        if(plane.Raycast(ray, out float distance))
+        {
+            worldPosition = ray.GetPoint(distance);
+        }
+        transform.position = worldPosition;
+        //float distance;
+        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //if (plane.Raycast(ray, out distance))
+        //{
+        //    worldPosition = ray.GetPoint(distance);
+        //}
     }
-    
 }
